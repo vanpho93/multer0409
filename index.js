f1c70f9f098ff366542c8c35ccaf0c41f8f446a0
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-29 18:30:53 +0700

    naming file

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const reload = require('reload');
-const upload = require('multer')({ dest: './public' });
+const multer = require('multer');
+
+// filename = username + milisecond + extension
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => cb(null, './public'),
+    filename:(req, file, cb) => {
+        const { originalname } = file;
+        const startIndex = originalname.lastIndexOf('.') + 1;
+        const extension = originalname.substring(startIndex);
+        const { username } = req.body;
+        const milisecond = Date.now();
+        cb(null, `${username}${milisecond}.${extension}`);
+    }
+});
+
+const upload = multer({ storage });
 
 const app = express();
 
@@ -14,6 +30,7 @@ app.post('/signup', upload.single('avatar'), (req, res) => {
     console.log(req.file);
     res.send(req.body);
 });
+
 
 reload(app);
 
